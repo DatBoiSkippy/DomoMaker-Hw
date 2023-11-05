df14d98efa21f7cf285c44dad31c95ad8f4b2703
Alex <nothingnicehere@gmail.com>
2023-11-05 16:22:39 -0500

    Finished Domomaker B and added zip file for A

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -10,7 +10,8 @@ const signupPage = (req, res) => {
 };
 
 const logout = (req, res) => {
-    return res.redirect('/');
+    req.session.destroy();
+    res.redirect('/');
 };
 
 const login = (req, res) => {
@@ -25,6 +26,8 @@ const login = (req, res) => {
         if (err || !account) {
             return res.status(401).json({ error: 'Wrong username or password' });
         }
+
+        req.session.account = Account.toAPI(account);
 
         return res.json({ redirect: '/maker' });
     });
@@ -47,11 +50,12 @@ const signup = async (req, res) => {
         const hash = await Account.generateHash(pass);
         const newAccount = new Account({ username, password: hash });
         await newAccount.save();
+        req.session.account = Account.toAPI(newAccount);
         return res.json({ redirect: '/maker' });
     } catch (err) {
         console.log(err);
         if (err.code === 11000) {
-            return res.status(400).json({ errpr: 'Username already in use!' });
+            return res.status(400).json({error: 'Username already in use!' });
         }
         return res.status(500).json({ error: 'An error occurred!' });
     }
