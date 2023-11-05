9177f98c49c446106701b3bb547e9fd643ef17e0
Alex <nothingnicehere@gmail.com>
2023-11-05 17:54:40 -0500

    fixed syntax error

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -14,7 +14,7 @@ const requiresLogout = (req, res, next) => {
 
 const requiresSecure = (req, res, next) => {
     if (req.headers['x-forwarded-proto'] !== 'https') {
-        return rs.redirect(`https://${req.hostname}${req.url}`);
+        return res.redirect(`https://${req.hostname}${req.url}`);
     }
     return next();
 }
